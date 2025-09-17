d9141c10d468c9b2e93837e273ebf3f4bf130474
Artur Hryhorychenko <114551560+Enyuu@users.noreply.github.com>
2025-09-17 12:17:23 +0200

    Update dessert.py
    Updates Vault token tests

--- testing/custom_plugins_dir/dessert.py
+++ testing/custom_plugins_dir/dessert.py
@@ -12,3 +12,19 @@ class DessertDetector(RegexBasedDetector):
             re.IGNORECASE,
         ),
     )
+
+class VaultTokenDetector(RegexBasedDetector):
+    """Scans for Vault tokens."""
+
+    secret_type = "Vault Token"
+
+    denylist = [
+        # Service token: hvs.<string>
+        re.compile(r'hvs\.[A-Za-z0-9\-_]{20,50}(?!\w)'),
+
+        # Batch token: hvb.<string>
+        re.compile(r'hvb\.[A-Za-z0-9\-_]{20,50}(?!\w)'),
+
+        # Recovery token: hvb.<string>
+        re.compile(r'hvr\.[A-Za-z0-9\-_]{20,50}(?!\w)'),
+    ]
